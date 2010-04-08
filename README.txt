439fe3b512ce648ee45e5148696f157edc07f08f
sgillies <sgillies@localhost>
2010-04-08 12:51:52 +0200

    Support GeoJSON-like polygons as well as Shapely polygons. Factor out a PolygonPath constructor.

--- README.txt
+++ README.txt
@@ -1,11 +1,13 @@
 Descartes
 =========
 
-Use geometric objects as matplotlib paths and patches
+Use Shapely or GeoJSON-like geometric objects as matplotlib paths and patches
 
 Descartes is aimed at making it easier to use Shapely_ objects with matplotlib.
 
 Requires: Shapely 1.2, matplotlib.
+
+See examples/patches.py.
 
 .. _Shapely: http://gispython.org/lab/wiki/Shapely
 
--- descartes/patch.py
+++ descartes/patch.py
@@ -2,21 +2,48 @@ from matplotlib.patches import PathPatch
 from matplotlib.path import Path
 from numpy import asarray, concatenate, ones
 
-def PolygonPatch(polygon, **kwargs):
+
+class Polygon(object):
+    # Adapt Shapely or GeoJSON/geo_interface polygons to a common interface
+    def __init__(self, context):
+        self.context = context
+    @property
+    def geom_type(self):
+        return (getattr(self.context, 'geom_type', None) 
+                or self.context['type'])
+    @property
+    def exterior(self):
+        return (getattr(self.context, 'exterior', None) 
+                or self.context['coordinates'][0])
+    @property
+    def interiors(self):
+        return (getattr(self.context, 'interiors', [])
+                or self.context['coordinates'][1:])
+
+
+def PolygonPath(polygon):
+    """Constructs a compound matplotlib path from a Shapely or GeoJSON-like
+    object"""
+    this = Polygon(polygon)
+    assert this.geom_type == 'Polygon'
     def coding(ob):
         # The codes will be all "LINETO" commands, except for "MOVETO"s at the
         # beginning of each subpath
-        n = len(ob.coords)
+        n = len(getattr(ob, 'coords', None) or ob)
         vals = ones(n, dtype=Path.code_type) * Path.LINETO
         vals[0] = Path.MOVETO
         return vals
-    # Convert coordinates to path vertices. Objects produced by Shapely's
-    # analytic methods have the proper coordinate order, no need to sort.
     vertices = concatenate(
-                    [asarray(polygon.exterior)] 
-                    + [asarray(r) for r in polygon.interiors])
+                    [asarray(this.exterior)] 
+                    + [asarray(r) for r in this.interiors])
     codes = concatenate(
-                [coding(polygon.exterior)] 
-                + [coding(r) for r in polygon.interiors])
-    return PathPatch(Path(vertices, codes), **kwargs)
+                [coding(this.exterior)] 
+                + [coding(r) for r in this.interiors])
+    return Path(vertices, codes)
+
+
+def PolygonPatch(polygon, **kwargs):
+    """Constructs a matplotlib patch from a Shapely or GeoJSON-like
+    object with full support for polygon holes"""
+    return PathPatch(PolygonPath(polygon), **kwargs)
 
